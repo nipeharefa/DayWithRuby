4934de77a20afafbac72422926b20783eea96a4d
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2017-06-19 10:34:09 +0700

    add variable

---
+++ 006.variable.rb
@@ -0,0 +1,24 @@
+# NOTE
+# Indonesia
+
+# ref https://www.tutorialspoint.com/ruby/ruby_variables.htm
+
+# $variable
+# Variable ini bersifat global bisa diakses dimana saja.
+# 
+# 
+# @variable
+# Variable ini mmerupakan instance dari suatu class, mehtod, atau parameter.
+# 
+# 
+# @@variable
+# Variabel ini merupakan Class variable.
+# 
+# variable
+# Merupakan variable local
+# 
+# 
+# __FILE___
+# mengembalilkan nama file yang sedang diesekusi
+# 
+# __LINE__ mengembalikan baris yang sedang diurutkan
